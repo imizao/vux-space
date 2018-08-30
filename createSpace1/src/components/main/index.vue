d2ed6acfae8862018ad9b06d529afdd6106f3ad1
apple <2313466338@qq.com>
2018-08-30 14:03:32 +0800

    add code

--- createSpace1/src/components/main/index.vue
+++ createSpace1/src/components/main/index.vue
@@ -4,30 +4,59 @@
     <a-tab></a-tab>
     <a-swiper></a-swiper>
     <router-view></router-view>
-    <!-- <m-content></m-content> -->
-    <!-- <spinner type="ripple"></spinner> -->
+    <transition :name="transitionName">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import { cookie } from 'vux'
- import aHead from '@/components/header'
- import aTab from '@/components/tab'
- import aSwiper from '@/components/swiper'
-//  import mContent from './mcontent'
+import { cookie } from "vux";
+import aHead from "@/components/header";
+import aTab from "@/components/tab";
+import aSwiper from "@/components/swiper";
 
- export default {
-   components: {
-     aHead,
-     aTab,
-     aSwiper
-   }
- }
+export default {
+  components: {
+    aHead,
+    aTab,
+    aSwiper
+  },
+  data() {
+    return {
+      transitionName: "slide-left"
+    };
+  },
+  watch: {
+    $route(to, from) {
+      let isBack = this.$router.isBack; //  监听路由变化时的状态为前进还是后退
+      if (isBack) {
+        this.transitionName = "slide-right";
+      } else {
+        this.transitionName = "slide-left";
+      }
+      this.$router.isBack = false;
+    }
+  }
+};
 </script>
 
 <style>
 .swiper-demo-img img {
   width: 100%;
+}
+.slide-left-enter,
+.slide-right-leave-active {
+  opacity: 0;
+  -webkit-transform: translate(100%, 0);
+  transform: translate(100%, 0);
+}
+
+.slide-left-leave-active,
+.slide-right-enter {
+  opacity: 1;
+  -webkit-transform: translate(-100%, 0);
+  transform: translate(-100% 0);
 }
 </style>
 
--- createSpace1/src/main.js
+++ createSpace1/src/main.js
@@ -2,24 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-// import VueRouter from 'vue-router'
 import axios from 'axios'
 import App from './App'
 import router from './router/index'
 import store from './store/index'
-
-// import Home from './components/HelloFromVux'
-
-// Vue.use(VueRouter)
-
-// const routes = [{
-//   path: '/',
-//   component: Home
-// }]
-
-// const router = new VueRouter({
-//   routes
-// })
 
 Vue.prototype.$http= axios
 FastClick.attach(document.body)
